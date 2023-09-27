83cc4b0ad2c3dea87683dfdaf5cb984c3c83eb94
Breno Macedo <brenonsmacedo@gmail.com>
2023-09-27 17:29:56 -0300

    pegarid-alterar

--- demo/src/main/java/br/edu/uerr/demo/controller/PessoaController.java
+++ demo/src/main/java/br/edu/uerr/demo/controller/PessoaController.java
@@ -1,22 +1,41 @@
 package br.edu.uerr.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.uerr.demo.model.Pessoa;
+import br.edu.uerr.demo.repository.PessoaRepository;
+import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @RestController
 @RequestMapping("/api/exemplo")
 
+
 public class PessoaController {
-    
+
+    @Autowired
+    PessoaRepository pessoaRepository;
+
     @GetMapping(value = "/{nome}")
     public String exemplo(@PathVariable("nome")String nome){
         return "Ola "+ nome;
     }
+    @GetMapping(value = "/pessoa/{id}")
+    public ResponseEntity<Pessoa> pegaPessoaPeloId(@PathVariable Integer id){
+        return pessoaRepository.findById(id)
+            .map(record->ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
+    }
 
-    public String salvar(){
-        return null;
+    @PostMapping(value = "/pessoa")
+    public Pessoa salvar(@RequestBody Pessoa pessoa){
+        return pessoaRepository.save(pessoa);
     }
 
     public String alterar(){
---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into PESSOA (id, nome, cep, logradouro, numero_casa, bairro, cidade, uf, telefone, email) 
+values (1, 'Breno', '69306402', 'rua das palmeiras', '452', 'centro', 'boa vista', 'RR', '95991368781', 'gelsonbarros57@gmail.com');
+INSERT INTO PESSOA (id, nome, cep, logradouro, numero_casa, bairro, cidade, uf, telefone, email) 
+VALUES (2, 'Maria Santos', '54321-876', 'Avenida B', 456, 'Jardim', 'Rio de Janeiro', 'RJ', '(21) 9876-5432', 'maria.santos@email.com');
+INSERT INTO PESSOA (id, nome, cep, logradouro, numero_casa, bairro, cidade, uf, telefone, email) 
+VALUES (3, 'Carlos Oliveira', '67890-123', 'Rua C', 789, 'Vila', 'Belo Horizonte', 'MG', '(31) 5678-9012', 'carlos.oliveira@email.com');
+INSERT INTO PESSOA (id, nome, cep, logradouro, numero_casa, bairro, cidade, uf, telefone, email) 
+VALUES (4, 'Ana Costa', '98765-432', 'Avenida D', 234, 'Praia', 'Fortaleza', 'CE', '(85) 4321-8765', 'ana.costa@email.com');
+INSERT INTO PESSOA (id, nome, cep, logradouro, numero_casa, bairro, cidade, uf, telefone, email) 
+VALUES (5, 'Pedro Pereira', '34567-890', 'Rua E', 567, 'Lagoa', 'Porto Alegre', 'RS', '(51) 6789-0123', 'pedro.pereira@email.com');
