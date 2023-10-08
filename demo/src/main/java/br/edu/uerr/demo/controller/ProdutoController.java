886cc7b3e39deed5a03ec9a845c1237ce06f6f1c
Breno Macedo <brenonsmacedo@gmail.com>
2023-10-08 01:32:43 -0300

    ControllerUpdate

--- demo/src/main/java/br/edu/uerr/demo/controller/ProdutoController.java
+++ demo/src/main/java/br/edu/uerr/demo/controller/ProdutoController.java
@@ -3,9 +3,11 @@ package br.edu.uerr.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -41,15 +43,29 @@ public class ProdutoController {
         return produtoRepository.save(produto);
     }
 
-    public String alterar(){
-        return null;
+    @PutMapping(value = "/produto/{id}")
+    public ResponseEntity<Produto> alterarProduto(@PathVariable("id")Integer id, @RequestBody Produto produto){
+        return produtoRepository.findById(id)
+            .map(record->{
+                record.setNome(produto.getNome());
+                record.setDescricao(produto.getDescricao());
+                record.setPreco(produto.getPreco());
+                record.setQuantidade(produto.getQuantidade());
+                record.setDataCompra(produto.getDataCompra());
+                Produto alteracao = produtoRepository.save(record);
+                return ResponseEntity.ok().body(alteracao);
+            }).orElse(ResponseEntity.notFound().build());
     }
 
     public String listaTodos(){
         return null;
     }
-
-    public String deletar(){
-        return null;
+    @DeleteMapping(value = "/produto/{id}")
+    public ResponseEntity<Object> deletarProduto(@PathVariable("id") Integer id){
+        return produtoRepository.findById(id)
+            .map(record->{
+                produtoRepository.deleteById(id);
+                return ResponseEntity.ok().build();
+            }).orElse(ResponseEntity.notFound().build());
     }
 }
